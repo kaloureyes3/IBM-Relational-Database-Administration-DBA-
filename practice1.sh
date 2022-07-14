202967bc06a250040773d6d7cc9c7510449eeb29
Carlos Vázquez Reyes <69530244+kaloureyes3@users.noreply.github.com>
2022-07-14 10:04:29 +0300

    Update practice1.sh

--- practice1.sh
+++ practice1.sh
@@ -1,3 +1,5 @@
+#Create a shell script which takes the database name and back up directory as parameters and backsup the database as <dbname>_timestamp.sql in the backup directory. If the database doesn’t exist, it should display appropriate message. If the backup dir doesn’t exist, it should create one.
+
 dbname=$(mysql -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$1'" | grep $1)
 
 if [ ! -d $2 ]; then 
